c4ffa2aa5d5793cb722d1e0a90cdde339d8fe8c0
Lenovo <IT19184272@my.sliit.lk>
2020-08-18 23:47:01 +0530

    First commit

--- app/src/main/java/com/example/intentsproj/FirstActivity.java
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -2,6 +2,7 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,8 +22,14 @@ public class FirstActivity extends AppCompatActivity {
     public void msgSend(View view)
     {
         Intent intent= new Intent( this, SecondActivity.class );
-        num1=findViewById(R.id.txt_num1);
-        num2=findViewById(R.id.txt_num2);
-        intent.putExtra(num1,num2);
+        EditText num1=findViewById(R.id.txt_num1);
+        EditText num2=findViewById(R.id.txt_num2);
+        intent.putExtra("num1",num1.getText().toString());
+        intent.putExtra("num2",num2.getText().toString());
+        startActivity(intent);
+
+        Context context=getApplicationContext();
+        charSequence message="You just clicked the OK button";
+        int duration=
     }
 }
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.intentsproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
@@ -10,5 +11,51 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent=getIntent();
+        String n1=intent.getStringExtra("number1");
+        String n2=intent.getStringExtra("number2");
+
+        int num1,num2;
+
+        num1=Integer.parseInt(n1);
+        num2=Integer.parseInt(n2);
+
+        txt_number1=findViewById(R.id.txt_number1);
+        txt_number2=findViewById(R.id.txt_number2);
+        lbl_answer=findViewById(R.id.lbl_answer);
+
+        txt_number1.setText(n1);
+        txt_number2.setText(n2);
+
+        public void Add(View view)
+        {
+            answer=num1+num2;
+            lbl_answer.setText(Integer.toString(answer));
+        }
+
+        public void subtraction(View view)
+        {
+            answer=num1-num2;
+            lbl_answer.setText(Integer.toString(answer));
+        }
+
+        public void division(View view)
+        {
+            answer=num1/num2;
+            lbl_answer.setText(Integer.toString(answer));
+        }
+
+        public void multiplication(View view)
+        {
+            answer=num1*num2;
+            lbl_answer.setText(Integer.toString(answer));
+        }
+
+
     }
+
+
+
+
 }
